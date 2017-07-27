3cb30920df0535ce0f02ac8e1bfa66202565b1ac
mutouji <59022665@qq.com>
2017-07-27 05:35:09 -0700

    add token contract

--- contracts/delphyICO.sol
+++ contracts/delphyICO.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.9;
+pragma solidity 0.4.10;
 
 
 /// @title Abstract token contract - Functions to be implemented by token contracts.
@@ -8,7 +8,7 @@ contract Token {
     function approve(address spender, uint256 value) returns (bool success);
 
     // This is not an abstract function, because solc won't recognize generated getter functions for public variables as functions.
-    function totalSupply() constant returns (uint256 supply) {}
+    function totalSupply() constant returns (uint256 supply) ;
     function balanceOf(address owner) constant returns (uint256 balance);
     function allowance(address owner, address spender) constant returns (uint256 remaining);
 
@@ -20,17 +20,16 @@ contract DelphyICO {
     /*
      *  Events
      */
-    event BidSubmission(address indexed sender, uint256 amount);
+    event BuySubmission(address indexed sender, uint256 amount);
     
     /*
      *  Constants
      */
-    uint constant public STEP1_PERIOD = 2 days;
-    uint constant public STEP2_PERIOD = 4 days;
-    uint constant public STEP3_PERIOD = 7 days;
-    uint constant public TOTAL_PERIOD = 13 days;
+    uint constant public STEP1_ENDDAY = 2 days;
+    uint constant public STEP2_ENDDAY = 6 days;
+    uint constant public STEP3_ENDDAY = 13 days;
     
-    uint constant public MAX_TOKENS_SOLD = 18000000 * 10**18; // 18M
+    uint constant public MAX_TOKENS_SOLD = 5000000 * 10**18; // 5M
     
     /*
      *  Enums
@@ -51,11 +50,11 @@ contract DelphyICO {
     address public owner;
     uint public startBlock;
     uint public startTime;
-    uint public endTime;
-    uint public totalReceived;
-    uint public soldTokens;
-    mapping (address => uint)[3] public bids;
-    mapping (address => uint) public bidToken;
+    uint public endBlock;
+    uint public etherReceived;
+    uint public tokenSolded;
+    mapping (address => uint)[3] public buys;
+    mapping (address => uint) public tokens;
     Stages public stage;
      
     /*
@@ -77,6 +76,7 @@ contract DelphyICO {
     modifier isOwner() {
         require (msg.sender == owner);
             // Only owner is allowed to proceed
+            
         _;
     }
 
@@ -90,7 +90,6 @@ contract DelphyICO {
     /*
      *  Public functions
      */
-     
     function sub(uint256 x, uint256 y) constant internal returns (uint256 z) {
         assert((z = x - y) <= x);
     }
@@ -99,8 +98,6 @@ contract DelphyICO {
         return block.timestamp;
     }
 
-    // Each window is 23 hours long so that end-of-window rotates
-    // around the clock for all timezones.
     function dayFor(uint timestamp) constant returns (uint) {
         return timestamp < startTime
             ? 0
@@ -111,7 +108,6 @@ contract DelphyICO {
         return dayFor(time());
     }
     
-    /// @dev Contract constructor function sets owner.
      function DelphyICO(address _wallet)
         public
     {
@@ -148,6 +144,7 @@ contract DelphyICO {
         startBlock = block.number;
         startTime = block.timestamp;
     }
+    
     function startTrade()
         public
         isWallet
@@ -156,22 +153,22 @@ contract DelphyICO {
         stage = Stages.TradingStarted;
     }
     
-    function bid(address receiver)
+    function buy()
         public
         payable
         isValidPayload
         atStage(Stages.IcoStarted)
         returns (uint amount)
     {
-        receiver = msg.sender;
+        address receiver = msg.sender;
         amount = msg.value;
         
         uint dayth = today();
         uint scale = 500;
         uint i = 0;
         
-        if (dayth < STEP1_PERIOD) {
-        } else if (dayth < STEP2_PERIOD) {
+        if (dayth < STEP1_ENDDAY) {
+        } else if (dayth < STEP2_ENDDAY) {
             scale = 470;
             i = 1;
         } else {
@@ -179,47 +176,42 @@ contract DelphyICO {
             i = 2;
         }
         
-        uint leftToken = MAX_TOKENS_SOLD - soldTokens;
+        uint leftToken = MAX_TOKENS_SOLD - tokenSolded;
         uint willsoldToken = amount * scale;
         
         if (willsoldToken > leftToken) {
             willsoldToken = leftToken;
             amount = willsoldToken / scale;
             // Send change back to receiver address. In case of a ShapeShift bid the user receives the change back directly.
-            if (!receiver.send(msg.value - amount))
-                // Sending failed
-                throw;
+            require(receiver.send(msg.value - amount));
         }
         
         // Forward funding to ether wallet
-        if (amount == 0 || !wallet.send(amount))
-            // No amount sent or sending failed
-            throw;
+        require (amount != 0 && wallet.send(amount));
         
-        bids[i][receiver] += amount;
-        totalReceived += amount;
-        bidToken[receiver] += willsoldToken;
-        soldTokens += willsoldToken;
+        buys[i][receiver] += amount;
+        etherReceived += amount;
+        tokens[receiver] += willsoldToken;
+        tokenSolded += willsoldToken;
         
-        if (dayth >= STEP3_PERIOD || soldTokens >= MAX_TOKENS_SOLD) {
+        if (dayth >= STEP3_ENDDAY || tokenSolded >= MAX_TOKENS_SOLD) {
             finalizeIco();
         }
     }
     
-    function claimTokens(address receiver)
+    function claimTokens()
         public
         isValidPayload
         atStage(Stages.TradingStarted)
     {
-        if (receiver == 0)
-            receiver = msg.sender;
-        uint tokenCount = bidToken[receiver] ;
+        address receiver = msg.sender;
+        uint tokenCount = tokens[receiver] ;
         require(tokenCount > 0);
-        require((bids[0][receiver] * 500 +
-            bids[1][receiver] * 470 + 
-            bids[2][receiver] * 430) == tokenCount);
+        require((buys[0][receiver] * 500 +
+            buys[1][receiver] * 470 + 
+            buys[2][receiver] * 430) == tokenCount);
             
-        bidToken[receiver] = 0;
+        tokens[receiver] = 0;
         delphyToken.transfer(receiver, tokenCount);
     }
     
@@ -230,7 +222,7 @@ contract DelphyICO {
         private
     {
         stage = Stages.IcoEnded;
-        delphyToken.transfer(wallet, MAX_TOKENS_SOLD - soldTokens);
-        endTime = now;
+        delphyToken.transfer(wallet, MAX_TOKENS_SOLD - tokenSolded);
+        endBlock = block.number;
     }
 }
---
+++ contracts/delphyToken.sol
@@ -0,0 +1,146 @@
+pragma solidity ^0.4.10;
+
+
+/// @title Abstract token contract - Functions to be implemented by token contracts.
+contract Token {
+    function transfer(address to, uint256 value) returns (bool success);
+    function transferFrom(address from, address to, uint256 value) returns (bool success);
+    function approve(address spender, uint256 value) returns (bool success);
+
+    // This is not an abstract function, because solc won't recognize generated getter functions for public variables as functions.
+    function totalSupply() constant returns (uint256 supply) {}
+    function balanceOf(address owner) constant returns (uint256 balance);
+    function allowance(address owner, address spender) constant returns (uint256 remaining);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+
+contract StandardToken is Token {
+
+    /*
+     *  Data structures
+     */
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+    uint256 public _totalSupply;
+    function totalSupply() constant returns (uint256) {
+        return _totalSupply;
+    }
+
+    /*
+     *  Public functions
+     */
+    /// @dev Transfers sender's tokens to a given address. Returns success.
+    /// @param _to Address of token receiver.
+    /// @param _value Number of tokens to transfer.
+    /// @return Returns success of function call.
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool)
+    {
+        if (balances[msg.sender] < _value) {
+            // Balance too low
+            throw;
+        }
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    /// @dev Allows allowed third party to transfer tokens from one address to another. Returns success.
+    /// @param _from Address from where tokens are withdrawn.
+    /// @param _to Address to where tokens are sent.
+    /// @param _value Number of tokens to transfer.
+    /// @return Returns success of function call.
+    function transferFrom(address _from, address _to, uint256 _value)
+        public
+        returns (bool)
+    {
+        if (balances[_from] < _value || allowed[_from][msg.sender] < _value) {
+            // Balance or allowance too low
+            throw;
+        }
+        balances[_to] += _value;
+        balances[_from] -= _value;
+        allowed[_from][msg.sender] -= _value;
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    /// @dev Sets approved amount of tokens for spender. Returns success.
+    /// @param _spender Address of allowed account.
+    /// @param _value Number of approved tokens.
+    /// @return Returns success of function call.
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool)
+    {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    /*
+     * Read functions
+     */
+    /// @dev Returns number of allowed tokens for given address.
+    /// @param _owner Address of token owner.
+    /// @param _spender Address of token spender.
+    /// @return Returns remaining allowance for spender.
+    function allowance(address _owner, address _spender)
+        constant
+        public
+        returns (uint256)
+    {
+        return allowed[_owner][_spender];
+    }
+
+    /// @dev Returns number of tokens owned by given address.
+    /// @param _owner Address of token owner.
+    /// @return Returns balance of owner.
+    function balanceOf(address _owner)
+        constant
+        public
+        returns (uint256)
+    {
+        return balances[_owner];
+    }
+}
+
+
+/// @title Delphy token contract
+/// @author jsw
+contract DelphyToken is StandardToken {
+
+    /*
+     *  Token meta data
+     */
+    string constant public name = "Delphy Token";
+    string constant public symbol = "DPY";
+    uint8 constant public decimals = 18;
+
+    /*
+     *  Public functions
+     */
+    function DelphyToken(address ico, address[] owners, uint[] tokens)
+        public
+    {
+        require(ico != 0);
+        
+        _totalSupply = 100000000 * 10**18; // 1e
+        balances[ico] = 5000000 * 10**18;
+        Transfer(0, ico, balances[ico]);
+        
+        uint assignedTokens = balances[ico];
+        for (uint i=0; i<owners.length; i++) {
+            require (owners[i] != 0);
+            balances[owners[i]] += tokens[i];
+            Transfer(0, owners[i], tokens[i]);
+            assignedTokens += tokens[i];
+        }
+        require (assignedTokens == _totalSupply);
+    }
+}
